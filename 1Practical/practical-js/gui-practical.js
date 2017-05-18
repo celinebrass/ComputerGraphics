6424086f2e60ef22a00d446f547ea9b962aba83a
Celine Brass <vis16cb@msc32.tcs.cs.ox.ac.uk>
2017-05-18 15:48:45 +0100

    addeed key press event

--- 1Practical/practical-js/gui-practical.js
+++ 1Practical/practical-js/gui-practical.js
@@ -1,6 +1,7 @@
 /* 
    Practical 2 GUI values
 */
+   var gui;
 
    // gui values
    var guiParameters = 
@@ -12,30 +13,56 @@
 
    function initGUI(){
 
-      var gui = new dat.GUI();
-      gui.add( guiParameters, 'autoCam' ).name('AutoCam').onChange(function(autoCam){console.log(autoCam); autoCamToggle();}).listen();
-      gui.add( guiParameters, 'moveSpeed', 0.1, 1000.0 ).name('Move Speed');      
+      gui = new dat.GUI();
+      gui.add( guiParameters, 'autoCam' ).name('AutoCam').onChange(function(autoCam)	{console.log(autoCam); autoCamToggle();}).listen();
+      gui.add( guiParameters, 'moveSpeed', 0.1, 1000.0 ).name('Move Speed')
+	.onChange(function(speed){
+		updateSpeed(speed);
+	});      
       gui.add( guiParameters, 'resetView' ).name('Reset View');
 
       // by default keep gui open:
       gui.open();
    }
 
+   function updateSpeed() {
+	controls.autoRotateSpeed = (guiParameters.moveSpeed * 50);
+	controls.update();
+   }
    // ViewPos Reset:
+   //calls reset cam
    function viewReset(){
+	camera.position.set(0, 0 , 100);
+    	camera.rotation.set(0, 0, 0);
+	controls.update();
       // reset the view
    }
 
    // If autocam is off, reset move speed and cam pos
+   //toggle autorotate
    function autoCamToggle(){
+	//guiParameters.autoCam = !(guiParameters.autoCam);
+	if (guiParameters.autoCam) {
+		controls.autoRotate = true;
+	}
+	else {
+		controls.autoRotate = false; 
+	}
+	controls.update();
       // If autocam is off, reset move speed and cam pos, lookat and lookup
    }
 
-   function resetCam(){
+   /*function resetCam(){
+	console.log('reset');
+	controls.autoRotate = false; 
+	controls.update();
       // reset the camera pos, lookat and up
    }
 
    // spin around the whole scene if autocam is on
    function updateAutoCam(){
-      // spin around the scene if autocam is on
-   }
+	console.log('trying to make shit rotate')
+	controls.autoRotate = true;
+	controls.update();
+	
+   }*/
--- 1Practical/practical-js/keyboard-practical.js
+++ 1Practical/practical-js/keyboard-practical.js
@@ -5,7 +5,30 @@
    /////////////////////////
    // KEYBOARD & MOVEMENT //
    /////////////////////////
-   function getKeyInput(){
-      // insert keyboard input here!
-      
-   }
+  function getKeyInput(key){
+    // insert keyboard input here!
+
+   if (key == 88){
+      //x
+      console.log('Camera rotation is : ');
+      console.log('x : ' + camera.rotation.x);
+      console.log('y : ' + camera.rotation.y);
+      console.log('z : ' + camera.rotation.z);
+    }   
+
+    else if (key == 80){
+      //p
+      console.log('Camera position is : ');
+      console.log('x : ' + camera.position.x);
+      console.log('y : ' + camera.position.y);
+      console.log('z : ' + camera.position.z);
+    }
+    //r
+    else if (key == 82) {
+    	viewReset();
+    }
+    //a
+    else if (key==65) {
+      camera.rotation.x -= 100;
+    }
+  }
