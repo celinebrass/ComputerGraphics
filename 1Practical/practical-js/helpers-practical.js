cd2a08b4121b17e67a18a2291976a23ef52a3f2e
Celine Brass <cjb327@cornell.edu>
2017-05-25 11:20:54 +0100

    finished translating"

--- 1Practical/practical-js/helpers-practical.js
+++ 1Practical/practical-js/helpers-practical.js
@@ -1,8 +1,10 @@
-/* 
-   Practical 2 
+/*
+   Practical 2
 */
      // Axes to give directions
    function axesHelper(){
+     var axisHelper = new THREE.AxisHelper( 100 );
+     scene.add( axisHelper );
       // axis helper
    }
 
